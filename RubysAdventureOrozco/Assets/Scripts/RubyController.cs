7d4671f821cf909959ca3158e37e3e0b48d67ec9
F00l29 <608564@student.dusd.net>
2022-12-09 11:45:09 -0800

    Sprite animation
    animated Ruby and bot

--- RubysAdventureOrozco/Assets/Scripts/RubyController.cs
+++ RubysAdventureOrozco/Assets/Scripts/RubyController.cs
@@ -16,10 +16,15 @@ public class RubyController : MonoBehaviour
     float vertical;
     public float speed = 3.0f;
 
+    Animator animator;
+    Vector2 lookDirection = new Vector2(1, 0);
+
     // Start is called before the first frame update
     void Start()
     {
+        animator = GetComponent<Animator>();
         rigidbody2d = GetComponent<Rigidbody2D>();
+
         currentHealth = maxHealth;
     }
 
@@ -28,6 +33,18 @@ public class RubyController : MonoBehaviour
     {
         horizontal = Input.GetAxis("Horizontal");
         vertical = Input.GetAxis("Vertical");
+
+        Vector2 move = new Vector2(horizontal, vertical);
+
+        if(!Mathf.Approximately(move.x, 0.0f) || !Mathf.Approximately(move.y, 0.0f))
+        {
+            lookDirection.Set(move.x, move.y);
+            lookDirection.Normalize();
+        }
+
+        animator.SetFloat("Look X", lookDirection.x);
+        animator.SetFloat("Look Y", lookDirection.y);
+        animator.SetFloat("Speed", move.magnitude);
 
         if (isInvincible)
         {
@@ -50,6 +67,7 @@ public class RubyController : MonoBehaviour
     {
         if (amount < 0)
         {
+            animator.SetTrigger("Hit");
             if (isInvincible)
                 return;
             isInvincible = true;
